0fab21e058ae5590cdee16e263539243bca78d23
Smit Shah <101961910+smit-shah-GG@users.noreply.github.com>
2023-12-20 16:25:30 +0530

    Working

--- runner.py
+++ runner.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, jsonify
-from langchain.llms import OpenAI
-from langchain import PromptTemplate, LLMChain
 from flask_cors import CORS, cross_origin
 import time
 import requests
@@ -18,12 +16,12 @@ headers = {
 # MAKE CHANGES HERE 
 #  |   |   |
 #  v   v   v
-servers = ['ip1', 'ip2']
+servers = ['localhost']
 
 while True:
     #asker
     for server in servers:
-        url = "http://" + server + ":5053/make_ask"
+        url = "http://" + server + ":5054/make_ask"
         payload = json.dumps({
             "server_name": "Myserver",
             "ip": "192.168.0.146"
@@ -33,8 +31,8 @@ while True:
             json_response = json.loads(response.text)
         
         if not response:
-            url2 = "http://" + server + ":5053/analyse_cpu"
-            url3 = "http://" + server + ":5053/analyse_mem"
+            url2 = "http://" + server + ":5056/analyse_cpu"
+            url3 = "http://" + server + ":5056/analyse_mem"
 
             payload = json.dumps({
                 "server_name": "Myserver",
@@ -46,15 +44,18 @@ while True:
             if str(response2) != "No outliers" or str(response3) != "No outliers found":
                 url4 = "http://192.168.74.1:3600/notify"
                 payload = json.dumps({
-                    "Outliers_cpu": json.loads(response2),
-                    "Outliers_mem": json.loads(response3)
+                    "Outliers_cpu": str(response2),
+                    "Outliers_mem": str(response3)
                 })
 
-                response4 = requests.request("POST", url2, headers=headers, data=payload)
+                print(payload)
+                # response4 = requests.request("POST", url2, headers=headers, data=payload)
 
             #if no reply, `
         #outliers
         #if outliers,
         #hit email 192 168 74 1 3600 /notify 
+                
+    print("Running...........")
 
     time.sleep(10)
